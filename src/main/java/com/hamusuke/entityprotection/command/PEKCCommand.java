d020a9f3a9e7f06415706cf3321408c270dddd40
ハムスケ ハム <daiki.esdfth12488@gmail.com>
2021-05-14 22:45:24 +0900

    fixed code

--- src/main/java/com/hamusuke/entityprotection/command/PEKCCommand.java
+++ src/main/java/com/hamusuke/entityprotection/command/PEKCCommand.java
@@ -3,6 +3,7 @@ package com.hamusuke.entityprotection.command;
 import com.hamusuke.entityprotection.util.EntityInvoker;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.command.argument.EntityArgumentType;
 import net.minecraft.entity.Entity;
 import net.minecraft.server.command.CommandManager;
@@ -10,68 +11,54 @@ import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.text.*;
 
 import java.util.Collection;
+import java.util.stream.Collectors;
 
 public class PEKCCommand {
     public static void register(CommandDispatcher<ServerCommandSource> dispatcher) {
         LiteralArgumentBuilder<ServerCommandSource> literalArgumentBuilder = CommandManager.literal("pekc").requires((source) -> source.hasPermissionLevel(2));
 
-        literalArgumentBuilder.then(CommandManager.literal("protect").then(CommandManager.argument("targets", EntityArgumentType.entities()).executes((context) -> protect(context.getSource(), EntityArgumentType.getEntities(context, "targets")))));
+        literalArgumentBuilder.then(CommandManager.literal("protect").then(CommandManager.argument("targets", EntityArgumentType.entities()).executes((context) -> protect(context.getSource(), EntityArgumentType.getEntities(context, "targets").stream().filter(entity -> ((EntityInvoker) entity).isKillable()).collect(Collectors.toList())))));
 
-        literalArgumentBuilder.then(CommandManager.literal("unprotect").then(CommandManager.argument("targets", EntityArgumentType.entities()).executes((context) -> unprotect(context.getSource(), EntityArgumentType.getEntities(context, "targets")))));
+        literalArgumentBuilder.then(CommandManager.literal("unprotect").then(CommandManager.argument("targets", EntityArgumentType.entities()).executes((context) -> unprotect(context.getSource(), EntityArgumentType.getEntities(context, "targets").stream().filter(entity -> !((EntityInvoker) entity).isKillable()).collect(Collectors.toList())))));
 
         literalArgumentBuilder.then(CommandManager.literal("isProtected").then(CommandManager.argument("targets", EntityArgumentType.entities()).executes((context) -> isProtected(context.getSource(), EntityArgumentType.getEntities(context, "targets")))));
 
         dispatcher.register(literalArgumentBuilder);
     }
 
-    private static int protect(ServerCommandSource source, Collection<? extends Entity> targets) {
-        int i = 0;
-        Entity e = null;
+    private static int protect(ServerCommandSource source, Collection<? extends Entity> targets) throws CommandSyntaxException {
+        targets.forEach(entity -> ((EntityInvoker) entity).setKillable(false));
 
-        for (Entity entity : targets) {
-            EntityInvoker entityInvoker = (EntityInvoker) entity;
-            if (entityInvoker.isKillable()) {
-                entityInvoker.setKillable(false);
-                i++;
-                e = entity;
-            }
-        }
-
-        if (i == 1) {
-            source.sendFeedback(new TranslatableText("command.pekc.protect.success.single", e.getDisplayName()), true);
+        if (targets.size() == 1) {
+            source.sendFeedback(new TranslatableText("command.pekc.protect.success.single", targets.iterator().next().getDisplayName()), true);
+        } else if (targets.size() > 1) {
+            source.sendFeedback(new TranslatableText("command.pekc.protect.success.multiple", targets.size()), true);
         } else {
-            source.sendFeedback(new TranslatableText("command.pekc.protect.success.multiple", i), true);
+            throw EntityArgumentType.ENTITY_NOT_FOUND_EXCEPTION.create();
         }
 
-        return i;
+        return targets.size();
     }
 
-    private static int unprotect(ServerCommandSource source, Collection<? extends Entity> targets) {
-        int i = 0;
-        Entity e = null;
+    private static int unprotect(ServerCommandSource source, Collection<? extends Entity> targets) throws CommandSyntaxException {
+        targets.forEach(entity -> ((EntityInvoker) entity).setKillable(true));
 
-        for (Entity entity : targets) {
-            EntityInvoker entityInvoker = (EntityInvoker) entity;
-            if (!entityInvoker.isKillable()) {
-                entityInvoker.setKillable(true);
-                i++;
-                e = entity;
-            }
-        }
-
-        if (i == 1) {
-            source.sendFeedback(new TranslatableText("command.pekc.unprotect.success.single", e.getDisplayName()), true);
+        if (targets.size() == 1) {
+            source.sendFeedback(new TranslatableText("command.pekc.unprotect.success.single", targets.iterator().next().getDisplayName()), true);
+        } else if (targets.size() > 1) {
+            source.sendFeedback(new TranslatableText("command.pekc.unprotect.success.multiple", targets.size()), true);
         } else {
-            source.sendFeedback(new TranslatableText("command.pekc.unprotect.success.multiple", i), true);
+            throw EntityArgumentType.ENTITY_NOT_FOUND_EXCEPTION.create();
         }
 
-        return i;
+        return targets.size();
     }
 
-    private static int isProtected(ServerCommandSource source, Collection<? extends Entity> targets) {
-        for (Entity entity : targets) {
-            EntityInvoker entityInvoker = (EntityInvoker) entity;
-            source.sendFeedback(new TranslatableText("command.pekc.isprotected." + !entityInvoker.isKillable() + ".success", entity.getDisplayName(), new LiteralText(entity.getUuidAsString()).setStyle(Style.EMPTY.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new TranslatableText("chat.copy.click"))).withClickEvent(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, entity.getUuidAsString())))), false);
+    private static int isProtected(ServerCommandSource source, Collection<? extends Entity> targets) throws CommandSyntaxException {
+        targets.forEach(entity -> source.sendFeedback(new TranslatableText("command.pekc.isprotected." + !((EntityInvoker) entity).isKillable() + ".success", entity.getDisplayName(), new LiteralText(entity.getUuidAsString()).setStyle(Style.EMPTY.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new TranslatableText("chat.copy.click"))).withClickEvent(new ClickEvent(ClickEvent.Action.COPY_TO_CLIPBOARD, entity.getUuidAsString())))), false));
+
+        if (targets.size() == 0) {
+            throw EntityArgumentType.ENTITY_NOT_FOUND_EXCEPTION.create();
         }
 
         return targets.size();
--- src/main/java/com/hamusuke/entityprotection/mixin/KillCommandMixin.java
+++ src/main/java/com/hamusuke/entityprotection/mixin/KillCommandMixin.java
@@ -1,6 +1,8 @@
 package com.hamusuke.entityprotection.mixin;
 
 import com.hamusuke.entityprotection.event.EntityKillCommandEvent;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import net.minecraft.command.argument.EntityArgumentType;
 import net.minecraft.entity.Entity;
 import net.minecraft.server.command.KillCommand;
 import net.minecraft.server.command.ServerCommandSource;
@@ -15,7 +17,7 @@ import java.util.Collection;
 @Mixin(KillCommand.class)
 public class KillCommandMixin {
     @Inject(at = @At(value = "HEAD"), method = "execute", cancellable = true)
-    private static void execute(ServerCommandSource source, Collection<? extends Entity> targets, CallbackInfoReturnable<Integer> cir) {
+    private static void execute(ServerCommandSource source, Collection<? extends Entity> targets, CallbackInfoReturnable<Integer> cir) throws CommandSyntaxException {
         int i = 0;
         Entity e = null;
 
@@ -29,8 +31,10 @@ public class KillCommandMixin {
 
         if (i == 1) {
             source.sendFeedback(new TranslatableText("commands.kill.success.single", e.getDisplayName()), true);
-        } else {
+        } else if (i > 1) {
             source.sendFeedback(new TranslatableText("commands.kill.success.multiple", i), true);
+        } else {
+            throw EntityArgumentType.ENTITY_NOT_FOUND_EXCEPTION.create();
         }
 
         cir.setReturnValue(i);
